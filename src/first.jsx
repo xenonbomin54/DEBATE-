2a4b376208a20e2f784da0034d3c21926c284c2a
xenonbomin54 <xenonbomin54@gmail.com>
2026-06-06 23:27:14 +0900

    초기 화면 일부 구현(3)

--- src/first.jsx
+++ src/first.jsx
@@ -33,6 +33,28 @@ export default function First() {
       <div style={{ padding: '20px', borderRadius: '8px', backgroundColor: 'rgba(255, 255, 255, 0.7)', width: '300px', textAlign: 'center' }}>
         <h1>논쟁의 즐거움, DEBATE!</h1>
         <p>환영합니다 "{email}"님<br />지금 바로 토론을 시작해보세요</p>
+        <button style={{
+      marginTop: '10px',
+      padding: '10px 20px',
+      borderRadius: '4px',
+      backgroundColor: '#00c8ff',
+      color: '#FFFFFF',
+      border: 'none',
+      fontSize: '16px',
+      cursor: 'pointer'
+    }}>광장 만들기</button><br />
+        <button style={{
+      marginTop: '10px',
+      marginBottom: '20px',
+      padding: '10px 20px',
+      borderRadius: '4px',
+      backgroundColor: '#00c8ff',
+      color: '#FFFFFF',
+      border: 'none',
+      fontSize: '16px',
+      cursor: 'pointer'
+    }}>광장 참여하기</button>
+        <br />
         <a href="#" onClick={handleLogout} style={{ color: '#000000', textDecoration: 'underline' }}>로그아웃</a>
       </div>
     </div>
