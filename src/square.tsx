0b9130cb0dba48ee3de1885c2ee74972f1516c30
xenonbomin54 <xenonbomin54@gmail.com>
2026-06-13 22:53:51 +0900

    패딩 스타일 수정

--- src/square.tsx
+++ src/square.tsx
@@ -15,7 +15,7 @@ function AAA({ posts, onMakeSquareClick, onPostClick }) {
   return (
     <div style={{ width: '45vw', height: '80vh', borderRadius: '15px', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
       <button style={{ fontSize: '5vh', border: 'none', cursor: 'pointer', borderRadius: '15px', backgroundColor: 'rgba(255, 255, 255, 0.7)', marginBottom: '50px' }} onClick={onMakeSquareClick}>+</button>
-      <div className="custom-scroll" style={{ overflowY: 'auto', flexGrow: 1, paddingRight: '10px' }}>
+      <div className="custom-scroll" style={{ overflowY: 'auto', flexGrow: 1, paddingRight: '30px' }}>
         <style>{`
           .custom-scroll::-webkit-scrollbar { width: 8px; }
           .custom-scroll::-webkit-scrollbar-track { background: rgba(0, 0, 0, 0.05); border-radius: 10px; }
